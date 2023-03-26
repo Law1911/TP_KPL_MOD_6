2f8fe21128832187fc09f25f16e88ced5523bcbd
Law1911 <epensusanto@gmail.com>
2023-03-27 03:58:33 +0700

    implementasi design by contract

--- TP_MOD_6_1302210106/Program.cs
+++ TP_MOD_6_1302210106/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace TP_MOD_6_1302210106
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo vid1= new SayaTubeVideo("Tutorial Design By Contract - Stevent Rangga Ramaditya");
+            vid1.IncreasePlayCount(200);
+            vid1.PrintVideoDetails();
+
+            Console.WriteLine("\n");
+            SayaTubeVideo vid2 = new SayaTubeVideo("Belajar Bahasa C#");
+
+            vid2.IncreasePlayCount(1000000000);
+            vid2.PrintVideoDetails();
+        }
+    }
+}
